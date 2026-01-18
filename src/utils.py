162718771aae5224ee226b4ea93b83d568b03822
Prachi221b <prachighosh08@gmail.com>
2026-01-18 20:11:11 +0530

    model training

--- src/utils.py
+++ src/utils.py
@@ -22,31 +22,32 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+        for model_name, model in models.items():
+            params = param[model_name]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(
+                estimator=model,
+                param_grid=params,
+                cv=3,
+                scoring="r2",
+                n_jobs=-1
+            )
 
-            # model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            gs.fit(X_train, y_train)
 
-            #model.fit(X_train, y_train)  # Train model
+            best_model = gs.best_estimator_
 
-            y_train_pred = model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+            test_score = r2_score(y_test, y_test_pred)
 
-            y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
-
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = {
+                "score": test_score,
+                "model": best_model
+            }
 
         return report
 
